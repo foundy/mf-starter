66a46282fddbc95510cc214f8d4b0cdf72feb65c
Foundy <afoundy@gmail.com>
2025-10-14 18:59:52 +0900

    feat: Test configuration for standalone execution

--- apps/rsbuild-host/rsbuild.config.ts
+++ apps/rsbuild-host/rsbuild.config.ts
@@ -1,6 +1,23 @@
+import { pluginModuleFederation } from '@module-federation/rsbuild-plugin';
 import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
 
 export default defineConfig({
-  plugins: [pluginReact()],
+  server: {
+    port: 3000,
+  },
+  plugins: [
+    pluginReact(),
+    pluginModuleFederation({
+      name: 'rsbuild-host',
+      remotes: {
+        '@apps/remote1': 'rsbuild-remote1@http://localhost:3001/mf-manifest.json',
+        '@apps/remote2': 'rsbuild-remote2@http://localhost:3002/mf-manifest.json',
+      },
+      shared: {
+        react: { singleton: true, requiredVersion: '^19.1.1' },
+        'react-dom': { singleton: true, requiredVersion: '^19.1.1' },
+      },
+    }),
+  ],
 });
--- apps/rsbuild-remote1/rsbuild.config.ts
+++ apps/rsbuild-remote1/rsbuild.config.ts
@@ -1,6 +1,27 @@
+import { pluginModuleFederation } from '@module-federation/rsbuild-plugin';
 import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
 
 export default defineConfig({
-  plugins: [pluginReact()],
+  server: {
+    port: 3001,
+  },
+  source: {
+    entry: {
+      index: './index.ts',
+    },
+  },
+  plugins: [
+    pluginReact(),
+    pluginModuleFederation({
+      name: 'rsbuildRemote1',
+      exposes: {
+        './App': './src/App.tsx',
+      },
+      shared: {
+        react: { singleton: true, requiredVersion: '^19.1.1' },
+        'react-dom': { singleton: true, requiredVersion: '^19.1.1' },
+      },
+    }),
+  ],
 });
